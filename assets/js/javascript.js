bbf13e17e2a3c00dc9e8491221aea42db0c65ec0
JoshRudge22 <>
2023-09-20 15:38:23 +0000

    finished off the java page, will finish off adding question then move on to the css page

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,7 +1,4 @@
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-
+(function(){
 function createQuiz(){
     const output = [];
 
@@ -11,103 +8,121 @@ function createQuiz(){
             for(letter in currentQuestion.answers){
                 answers.push(
                     `<label>
-                    <input type="radio" name="question${questionNumber}"value="
+                    <input type="radio" name="question${questionNumber}"value="${letter}">
+                    ${letter} :
+                    ${currentQuestion.answers[letter]}
                     </label>`
-                )
+                );
             }
+    output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`
+    );
         }
-    )
+    );
+    quizContainer.innerHTML = output.join('');
 }
 
-function showResults(){}
+function showResults(){
+    const answerContainers = quizContainer.querySelectAll('.answers');
+    
+    let nuCorrect = 0;
+    
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+        
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=questions${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-createQuiz();
+        if(userAnswer === currentQuestion.correctAnswer){
+            numCorrect++;
+            answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        else{
+            answerContainers[questionNumber].style.color = 'red';
+        }
+    });
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+}
 
-submitButton.addEventListener('click', showResults)
-/* Will add the questions later
-const questions = [
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+
+const myQuestions = [
     {
-        question: {
+        question: "Who won the frist ever Premier League Trophy in the 1992-93 season?",
             answers: {
-                a:
-                b:
-                c:
-                d:
+                a: "Manchester United",
+                b: "Blackburn Rovers",
+                c: "Liverpool",
+                d: "Aresnal"
             },
-            correctAnswer:
+            correctAnswer: "a"
         },
     {
-        question: {
+        question: "In the season 1993-94 who became the most expensive footballer signed by an English Football team?",
             answers: {
-                a:
-                    b:
-                c:
-                    d:
+                a: "Andy Townsend",
+                b: "Roy Keane",
+                c: "Tim Flowers",
+                d: "Neil Ruddock"
             },
-            correctAnswer:
+            correctAnswer: "b"
         },
     {
-        question: {
+        question: "In the 1994-95 which club had the smallest Stadium?",
             answers: {
-                a:
-                    b:
-                c:
-                    d:
+                a: "Southampton-The Dell"
+                b: "Norwich-Carrow Road"
+                c: "Queens Park Rangers-Loftus Road"
+                d: "Ipswich Town-Portman Road"
             },
-            correctAnswer:
+            correctAnswer: "a"
         },
     {
-        question: {
+        question: "Blackburn Rovers recored the lowest ever finsh by a Premier League title holder. Where did they finish in 1995-96",
             answers: {
-                a:
-                    b:
-                c:
-                    d:
+                a: "9th",
+                b: "15th",
+                c: "4th",
+                d: "7th"
             },
-            correctAnswer:
+            correctAnswer: "d"
         },
     {
-        question: {
+        question: "In 1996-97 Wimbledon had to play their home games at which stadium?",
             answers: {
-                a:
-                    b:
-                c:
-                    d:
+                a: "Boleyn Ground",
+                b: "Stamford Bridge",
+                c: "Roker Park",
+                d: "Selhurst Park"
             },
-            correctAnswer:
+            correctAnswer: "d"
         },
     {
-        question: {
-            answers: {
-                a:
-                    b:
-                c:
-                    d:
+        question: "In 1997-98 the golden boot was shared between 3 players who each scored 18 goals, which player didn't share the golden boot?",
+            answers: 
+                a: "Andy Cole",
+                b: "Chris Sutton",
+                c: "Michael Owen",
+                d: "Dion Dublin"
             },
-            correctAnswer:
+            correctAnswer: "a"
         },
     {
-        question: {
+        question: "Which team had the longest unbeaten run in the 1998-99 season?",
             answers: {
-                a:
-                    b:
-                c:
-                    d:
+                a: "West Ham",
+                b: "Chelsea",
+                c: "Manchester United",
+                d: "Arsenal"
             },
-            correctAnswer:
+            correctAnswer: "b"
         },
     {
-        question: {
-            answers: {
-                a:
-                    b:
-                c:
-                    d:
-            },
-            correctAnswer:
-        },
-    {
-        question: {
+        question: 
             answers: {
                 a:
                     b:
@@ -317,7 +332,7 @@ const questions = [
             correctAnswer:
         },
     {
-        question: {
+        question: 
             answers: {
                 a:
                     b:
@@ -327,7 +342,17 @@ const questions = [
             correctAnswer:
         },
     {
-        question: {
+        question: 
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: 
             answers: {
                 a:
                     b:
@@ -337,5 +362,9 @@ const questions = [
             correctAnswer:
         },
     }
-]
-*/
+];
+
+createQuiz();
+
+submitButton.addEventListener('click', showResults)
+})();
