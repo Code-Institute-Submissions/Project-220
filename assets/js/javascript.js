a74959144408b5642a5b81b13e4afd47c589289f
JoshRudge22 <>
2023-09-20 09:38:53 +0000

    added an output variable to show all the questions and answers

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,5 +1,341 @@
-
-
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
+
+function createQuiz(){
+    const output = [];
+
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+            const answers = [];
+            for(letter in currentQuestion.answers){
+                answers.push(
+                    `<label>
+                    <input type="radio" name="question${questionNumber}"value="
+                    </label>`
+                )
+            }
+        }
+    )
+}
+
+function showResults(){}
+
+createQuiz();
+
+submitButton.addEventListener('click', showResults)
+/* Will add the questions later
+const questions = [
+    {
+        question: {
+            answers: {
+                a:
+                b:
+                c:
+                d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    {
+        question: {
+            answers: {
+                a:
+                    b:
+                c:
+                    d:
+            },
+            correctAnswer:
+        },
+    }
+]
+*/
